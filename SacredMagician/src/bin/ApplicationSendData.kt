89387f32cc19c80b54f078d4748e0fa061a69d50
Pavel Erokhin <39625750+MairwunNx@users.noreply.github.com>
2018-12-10 16:38:28 +0300

    Added check on internet connection in SendData.
    Also added fields of TextFields with name by fxid.

--- SacredMagician/src/bin/ApplicationSendData.kt
+++ SacredMagician/src/bin/ApplicationSendData.kt
@@ -10,31 +10,33 @@ import java.util.concurrent.Executors
 
 class ApplicationSendData {
     fun send() {
-        val f = File("\$SacredMagician\\conf\\app.stat.txt")
+        if (ApplicationSummary().isOnline) {
+            val f = File("\$SacredMagician\\conf\\app.stat.txt")
 
-        if (!f.exists() && !f.isDirectory) {
-            val executor = Executors.newSingleThreadExecutor()
+            if (!f.exists() && !f.isDirectory) {
+                val executor = Executors.newSingleThreadExecutor()
 
-            executor.submit {
-                val data = "DATA: [${LocalDateTime.now()}] | OS: [${System.getProperty("os.name")} \\ ${System.getProperty("user.name")}] | VER: [${ApplicationSummary().version} \\ ${ApplicationSummary().aVersion}] | TYPE: [${ApplicationSummary().type}]"
-                val dataEncoded = Base64.getEncoder().withoutPadding().encodeToString(data.toByteArray())
+                executor.submit {
+                    val data = "DATA: [${LocalDateTime.now()}] | OS: [${System.getProperty("os.name")} \\ ${System.getProperty("user.name")}] | VER: [${ApplicationSummary().version} \\ ${ApplicationSummary().aVersion}] | TYPE: [${ApplicationSummary().type}]"
+                    val dataEncoded = Base64.getEncoder().withoutPadding().encodeToString(data.toByteArray())
 
-                val url = URL("http://mnxtelemetry.zzz.com.ua/send.php?type=sm&data=$dataEncoded")
+                    val url = URL("http://mnxtelemetry.zzz.com.ua/send.php?type=sm&data=$dataEncoded")
 
-                try {
-                    val conn = url.openConnection()
-                    val inStream = conn.getInputStream()
-                    inStream.close()
+                    try {
+                        val conn = url.openConnection()
+                        val inStream = conn.getInputStream()
+                        inStream.close()
 
-                    f.createNewFile()
+                        f.createNewFile()
 
-                    ApplicationLogger().logger.info("SacredMagician anonymous statistics has been sent!")
-                } catch (e: Exception) {
-                    ApplicationLogger().logger.error(e.printStackTrace().toString())
+                        ApplicationLogger().logger.info("SacredMagician anonymous statistics has been sent!")
+                    } catch (e: Exception) {
+                        ApplicationLogger().logger.error(e.printStackTrace().toString())
+                    }
                 }
+            } else {
+                f.createNewFile()
             }
-        } else {
-            f.createNewFile()
         }
     }
 }
--- SacredMagician/src/bvw/BaseView.kt
+++ SacredMagician/src/bvw/BaseView.kt
@@ -1,9 +1,56 @@
 package bvw
 
 import ApplicationSummary
+import javafx.scene.control.TextField
 import javafx.scene.layout.BorderPane
 import tornadofx.*
 
 class BaseView : View("${ApplicationSummary().name} ${ApplicationSummary().aVersion}") {
     override val root: BorderPane by fxml("/wnd/BaseWindow.fxml")
+
+    private val balanceBinFileChanged = false
+
+    private val bronzeAwVwTextField: TextField by fxid("bronzeAwVwTextField")
+    private val bronzeHitPointsTextField: TextField by fxid("bronzeHitPointsTextField")
+    private val bronzeResistanceTextField: TextField by fxid("bronzeResistanceTextField")
+    private val bronzeDamageTextField: TextField by fxid("bronzeDamageTextField")
+
+    private val silverAwVwTextField: TextField by fxid("silverAwVwTextField")
+    private val silverHitPointsTextField: TextField by fxid("silverHitPointsTextField")
+    private val silverResistanceTextField: TextField by fxid("silverResistanceTextField")
+    private val silverDamageTextField: TextField by fxid("silverDamageTextField")
+
+    private val goldAwVwTextField: TextField by fxid("goldAwVwTextField")
+    private val goldHitPointsTextField: TextField by fxid("goldHitPointsTextField")
+    private val goldResistanceTextField: TextField by fxid("goldResistanceTextField")
+    private val goldDamageTextField: TextField by fxid("goldDamageTextField")
+
+    private val platinumAwVwTextField: TextField by fxid("platinumAwVwTextField")
+    private val platinumHitPointsTextField: TextField by fxid("platinumHitPointsTextField")
+    private val platinumResistanceTextField: TextField by fxid("platinumResistanceTextField")
+    private val platinumDamageTextField: TextField by fxid("platinumDamageTextField")
+
+    private val niobiumAwVwTextField: TextField by fxid("niobiumAwVwTextField")
+    private val niobiumHitPointsTextField: TextField by fxid("niobiumHitPointsTextField")
+    private val niobiumResistanceTextField: TextField by fxid("niobiumResistanceTextField")
+    private val niobiumDamageTextField: TextField by fxid("niobiumDamageTextField")
+
+    private val multiPlayerAwVwTextField: TextField by fxid("multiPlayerAwVwTextField")
+    private val multiPlayerHitPointsTextField: TextField by fxid("multiPlayerHitPointsTextField")
+    private val multiPlayerResistanceTextField: TextField by fxid("multiPlayerResistanceTextField")
+    private val multiPlayerDamageTextField: TextField by fxid("multiPlayerDamageTextField")
+
+    private val southCenterRegionTextField: TextField by fxid("southCenterRegionTextField")
+    private val northCenterRegionTextField: TextField by fxid("northCenterRegionTextField")
+    private val swampBaseRegionTextField: TextField by fxid("swampBaseRegionTextField")
+    private val westBaseRegionTextField: TextField by fxid("westBaseRegionTextField")
+    private val northBaseRegionTextField: TextField by fxid("northBaseRegionTextField")
+    private val lavaBaseRegionTextField: TextField by fxid("lavaBaseRegionTextField")
+    private val shaddarBaseRegionTextField: TextField by fxid("shaddarBaseRegionTextField")
+    private val upperUnderworldTextField: TextField by fxid("upperUnderworldTextField")
+    private val lowerBaseRegionTextField: TextField by fxid("lowerBaseRegionTextField")
+
+    init {
+
+    }
 }
