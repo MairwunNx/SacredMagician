ba68805865ad888bbc935da4e2351c06580fc877
Pavel Erokhin <39625750+MairwunNx@users.noreply.github.com>
2018-12-29 21:29:17 +0300

    Updated build number. Prepare to update utility.

--- SacredMagician/src/ApplicationSummary.kt
+++ SacredMagician/src/ApplicationSummary.kt
@@ -1,13 +1,13 @@
 class ApplicationSummary {
     companion object {
         const val name: String = "SacredMagician"
-        const val version: String = "1.0.1.0.261218.R2"
+        const val version: String = "1.0.1.1.291218.R3"
         const val aVersion: String = "1.0.0.0.151218.A1"
         const val type: String = "Release"
+        val slash: String = System.getProperty("file.separator")!!
         var maxLength: Int = 4
         var isOnline: Boolean = false
         var binPath: String = ""
         var startTime: Long = 0
-        val slash = System.getProperty("file.separator")!!
     }
 }
